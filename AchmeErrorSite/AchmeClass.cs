94bc505647d3260c296fd861c45fa52202e05a9c
Morten Bock <morten@mortenbock.dk>
2017-02-03 18:34:52 +0100

    Add basic site throwing errors

---
+++ AchmeErrorSite/AchmeClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AchmeErrorSite
+{
+	public class AchmeClass
+	{
+		public static void ThrowUp()
+		{
+			string foo = null;
+			foo.Split(',');
+		}
+	}
+}
